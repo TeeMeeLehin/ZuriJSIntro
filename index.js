ab54f5a48973f36f2b9f775a8dd243a73c7e2a02
TeeMeeLehin <59570534+TeeMeeLehin@users.noreply.github.com>
2022-06-22 16:44:31 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+let myname = 'Timilehin Olamijulo'
+let height = '5 ft 5"'
+let country = 'Nigeria'
+
+alert('My name is ' + myname)
+alert( 'Height: ' + height)
+alert('Country: ' + country)
+
+console.log('My name is ' + myname)
+console.log( 'Height: ' + height)
+console.log('Country: ' + country)
